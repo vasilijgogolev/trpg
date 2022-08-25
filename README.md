355d19ab6eb90667064eaa032c5e25f0b61b14a4
ripykt <ripykt@gmail.com>
2022-08-25 22:22:14 +0700

    added source code

---
+++ README.md
@@ -0,0 +1,9 @@
+# trpg
+
+## how to build
+
+1. Call `mkdir build`
+2. Then move to folder `cd build` 
+3. and then call cmake: `cmake .. -GXcode` 
+
+Currently supports only MacOS and XCode, but should be no problem to call `cmake .. -G"Visual Studio 16 2019" -AWin32` or something like that
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,111 @@
+#include <imgui.h>
+#include <imgui_impl_opengl2.h>
+#include <imgui_impl_glfw.h>
+#include <imgui_stdlib.h>
 #include <iostream>
 
-int main() {
-	std::cout << "Hello world!" << std::endl;
+#ifdef __APPLE__
+#define GL_SILENCE_DEPRECATION
+#endif
+#include <GLFW/glfw3.h>
+
+#ifdef WIN32
+#include <Windows.h>
+#endif
+
+#if defined(_MSC_VER) && (_MSC_VER >= 1900) && !defined(IMGUI_DISABLE_WIN32_FUNCTIONS)
+#pragma comment(lib, "legacy_stdio_definitions")
+#endif
+
+GLFWwindow* window = nullptr;
+bool windowOpen = true;
+
+static void glfw_error_callback(int error, const char* description)
+{
+	fprintf(stderr, "Glfw Error %d: %s\n", error, description);
+}
+
+void shutdown() {
+	ImGui_ImplOpenGL2_Shutdown();
+	ImGui_ImplGlfw_Shutdown();
+	ImGui::DestroyContext();
+	
+	glfwDestroyWindow(window);
+	glfwTerminate();
+}
+
+int main(int, char**)
+{
+	glfwSetErrorCallback(glfw_error_callback);
+	if (!glfwInit())
+		return 1;
+	glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+	window = glfwCreateWindow(1280, 720, "trpg", NULL, NULL);
+	if (window == NULL)
+		return 1;
+	glfwMakeContextCurrent(window);
+	glfwSwapInterval(1);
+	
+	IMGUI_CHECKVERSION();
+	ImGui::CreateContext();
+	ImGuiIO& io = ImGui::GetIO(); (void)io;
+	
+	io.FontGlobalScale = 1.3f;
+	
+	ImGui::StyleColorsDark();
+	
+	ImGui_ImplGlfw_InitForOpenGL(window, true);
+	ImGui_ImplOpenGL2_Init();
+	
+	ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+	
+	while (!glfwWindowShouldClose(window))
+	{
+		glfwPollEvents();
+		
+		ImGui_ImplOpenGL2_NewFrame();
+		ImGui_ImplGlfw_NewFrame();
+		ImGui::NewFrame();
+		
+		{
+			
+			ImGui::SetNextWindowPos(ImVec2(io.DisplaySize.x / 2.0f, io.DisplaySize.y / 2.0f), ImGuiCond_Once, ImVec2(0.5f, 0.5f));
+			ImGui::SetNextWindowSize(ImVec2(io.DisplaySize.x, io.DisplaySize.y), ImGuiCond_Once);
+			ImGui::Begin("trpg", &windowOpen, ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoNav | ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoTitleBar);
+			if (ImGui::BeginTabBar("MyTabBar")) {
+				
+				ImGui::EndTabBar();
+			}
+			
+			ImGui::End();
+		}
+		
+		
+		// Rendering
+		ImGui::Render();
+		int display_w, display_h;
+		glfwGetFramebufferSize(window, &display_w, &display_h);
+		glViewport(0, 0, display_w, display_h);
+		glClearColor(clear_color.x * clear_color.w, clear_color.y * clear_color.w, clear_color.z * clear_color.w, clear_color.w);
+		glClear(GL_COLOR_BUFFER_BIT);
+		
+			
+		ImGui_ImplOpenGL2_RenderDrawData(ImGui::GetDrawData());
+		
+		glfwMakeContextCurrent(window);
+		glfwSwapBuffers(window);
+	}
+	
+	shutdown();
+	
 	return 0;
 }
+
+#ifdef WIN32
+int APIENTRY WinMain(HINSTANCE hInstance,
+	HINSTANCE hPrevInstance,
+	LPSTR lpCmdLine, int nCmdShow)
+{
+	return main(__argc, __argv);
+}
+#endif
